ef1be940f2d106932402af2052ee2d538a82aca9
shahid-devil <89721494+shahid-devil@users.noreply.github.com>
2021-09-07 19:18:57 +0530

    Deadpool 😈

--- plugins/clear.js
+++ plugins/clear.js
@@ -25,5 +25,5 @@ MyPnky.addCommand({pattern: 'clear', fromMe: true, desc: END, dontAddCommandList
 
     await message.sendMessage('```cleaning chat...```');
     await message.client.modifyChat (message.jid, ChatModification.delete);
-    await message.sendMessage('```🏳 Chat cleared 🏳```');
+    await message.sendMessage('```👻 Chat cleared 👻```');
 }));
